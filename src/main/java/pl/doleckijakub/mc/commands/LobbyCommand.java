ee72f5bbd3eec8606c246fd28ee86860c1fb763d
SimpleBadger <122283207+Badgaar@users.noreply.github.com>
2024-05-01 14:00:23 +0200

    c2 - lobbycommand poprawione

--- src/main/java/pl/doleckijakub/mc/commands/LobbyCommand.java
+++ src/main/java/pl/doleckijakub/mc/commands/LobbyCommand.java
@@ -1,28 +1,15 @@
 package pl.doleckijakub.mc.commands;
-
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.ChatColor;
+import pl.doleckijakub.mc.minigames.Lobby;
 import org.bukkit.entity.Player;
 import pl.doleckijakub.mc.common.CommandInfo;
-import pl.doleckijakub.mc.common.Minigame;
-import pl.doleckijakub.mc.common.MinigameManager;
 import pl.doleckijakub.mc.common.PluginCommand;
 
-import javax.naming.InvalidNameException;
-import java.util.Map;
-import java.util.UUID;
+@CommandInfo(name = "lobby")
+public class PlayCommand extends PluginCommand {
 
-public class LobbyCommand {
-
-    @CommandInfo(name = "lobby")
-    public class PlayCommand extends PluginCommand {
-
-        @Override
-        public void tp(Player player, String[] args) {
-            player.teleport(lobby.getInstance().getWorld().getSpawnLocation());
-        }
-
+    @Override
+    public void execute(Player player, String[] args) {
+        player.teleport(Lobby.getInstance().getWorld().getSpawnLocation());
     }
 
 }

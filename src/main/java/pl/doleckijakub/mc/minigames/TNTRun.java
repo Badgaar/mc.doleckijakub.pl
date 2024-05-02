d6a5520d1a9fa8f2102dd4fed2878b3824047392
SimpleBadger <122283207+Badgaar@users.noreply.github.com>
2024-05-02 10:18:32 +0200

    TNT RUN (Created timer and block remover)

--- src/main/java/pl/doleckijakub/mc/minigames/TNTRun.java
+++ src/main/java/pl/doleckijakub/mc/minigames/TNTRun.java
@@ -2,13 +2,21 @@ package pl.doleckijakub.mc.minigames;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.Plugin;
 import pl.doleckijakub.mc.common.GameWorld;
 import pl.doleckijakub.mc.common.Minigame;
 
-public class TNTRun extends Minigame {
+import static org.bukkit.Bukkit.getServer;
+
+public class TNTRun extends Minigame{
+
 
     public enum GameState {
         LOBBY,
@@ -18,9 +26,12 @@ public class TNTRun extends Minigame {
         @Override
         public String toString() {
             switch (this) {
-                case LOBBY: return ChatColor.GREEN + "Lobby";
-                case RUNNING: return ChatColor.GOLD + "Running";
-                case FINISHED: return ChatColor.RED + "Finished";
+                case LOBBY:
+                    return ChatColor.GREEN + "Lobby";
+                case RUNNING:
+                    return ChatColor.GOLD + "Running";
+                case FINISHED:
+                    return ChatColor.RED + "Finished";
             }
 
             return null;
@@ -47,9 +58,10 @@ public class TNTRun extends Minigame {
     public void teleportPlayer(Player player) {
         switch (gameState) {
             case LOBBY: {
-                while (this.lobbyWorld == null);
+                while (this.lobbyWorld == null) ;
                 player.teleport(lobbyWorld.getWorld().getSpawnLocation());
-            } break;
+            }
+            break;
         }
     }
 
@@ -66,9 +78,55 @@ public class TNTRun extends Minigame {
     @Override
     public World getWorld() {
         switch (gameState) {
-            case LOBBY: return lobbyWorld.getWorld();
-            default: throw new RuntimeException("unimplemented");
+            case LOBBY:
+                return lobbyWorld.getWorld();
+            default:
+                throw new RuntimeException("unimplemented");
         }
     }
 
+    //1s=20tic
+    public int secondsToStart = 10;
+    String stringedSecondsToStart = ""+secondsToStart;
+
+    //Centered countdown
+    public void sendTimerMessage(Player player,int secondsToStart) {
+        int messageLength = stringedSecondsToStart.length();
+        int centerMessage = 154 / 2;
+        int messageStart = centerMessage - (messageLength / 2);
+        String spaces = "";
+        for (int i = 0; i < messageStart; i++) {
+            spaces += " ";
+        }
+        player.sendMessage(spaces + secondsToStart);
+    }
+
+    //Start countdown
+    //Weź proszę popraw tego zjebanego syntaxa w 105
+    private void startCountdown() {
+        Bukkit.getScheduler().runTaskTimer(TNTRun,() -> {
+            if (secondsToStart <= 0) {
+                return;
+            }
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                sendTimerMessage(player, secondsToStart);
+            }
+            secondsToStart--;
+        }, 0L, 20L);
+    }
+
+    //Block remover
+    @EventHandler
+    public void onPlayerMoveEvent(PlayerMoveEvent e) {
+        Player player = e.getPlayer();
+        //Get blocks (below player and one under it)
+        Block blockBelowPlayer = Player.getLocation().substract(0, 1, 0).getblock();
+        Block blockBelowBlockBelowPlayer = Player.getLocation().substract(0, 2, 0).getblock();
+
+        //If player is on the ground remove 2 block underneath his feet
+        if (blockBelowPlayer.getType() != Material.AIR && secondsToStart != 0) {
+            blockBelowPlayer.setType(Material.AIR);
+            blockBelowBlockBelowPlayer.setType(Material.AIR);
+        } else super.onPlayerMoveEvent(e);
+    }
 }
